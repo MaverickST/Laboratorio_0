79074cf86c06e1d471db8cb445b68713164ba9c4
MaverickST <maverick.sossa@udea.edu.co>
2021-03-15 19:53:55 -0500

    Modificaciones en el punto 2

--- Punto_2/Punto_2/main.cpp
+++ Punto_2/Punto_2/main.cpp
@@ -1,36 +1,20 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
 
 int main()
 {
-    /*
-    int nu=0,acomulado=0,lont=0;
-    string cad,cadaux;
 
-    cout<<"Ingrese un numero entero: ";
-    cin>>cad;
-
-    lont=cad.length(); //Encuentro la longitud de la cadena ingresada
-
-    for(int i=0; i<lont;i++){ //Itera sobre la longitud de la cadena
-        cadaux=cad[i]; //Obtengo el primer caracter, el primer numero
-        nu=stoi(cadaux);//Lo convierto a entero para operarlo
-        nu=pow(nu,nu);//Lo elevo a sí mismp
-        acomulado+=nu;//Sumo todas potencias.
-
-    }
-    cout<<acomulado<<endl;
-    */
-
-    int numero,n,aco=0;
+    int numero,n,aco=0,otron;
 
     cout<<"Ingrese el numero: ";
     cin>>numero;
 
     while(numero>0){
         n=numero%10;
-        n=pow(n,n);
+        otron=n;
+        for(int i=n;i>1;i--){
+            n=n*otron;
+        }
         aco+=n;
         numero=numero/10;
 
